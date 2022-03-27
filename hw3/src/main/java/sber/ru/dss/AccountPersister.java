13825a671083d0814a05bfd4d033914f151080df
IgorOzherelev <i9163073959@gmail.com>
2022-03-27 19:59:00 +0300

    hw3: small fix

--- hw3/src/main/java/sber/ru/dss/AccountPersister.java
+++ hw3/src/main/java/sber/ru/dss/AccountPersister.java
@@ -12,13 +12,17 @@ public class AccountPersister {
     private final List<PreparedStatement> preparedStatementsForUpdate;
     private final List<PreparedStatement> preparedStatementsForFindById;
 
-    public Account getAccountById(long fromId) {
-        return preparedStatementsForFindById
+    public Account getAccountById(long accountId) {
+        var list = preparedStatementsForFindById
                 .stream()
                 .map(preparedStatement -> AccountDao.getAccountById(
-                        preparedStatement, fromId))
+                        preparedStatement, accountId))
                 .filter(acc -> acc != null && acc.getId() != 0)
-                .toList().get(0);
+                .toList();
+
+        if (list.size() != 0) return list.get(0);
+        else throw new IllegalArgumentException("The account id: "
+                + accountId + " does not exist");
     }
 
     public void updateAccountBalance(long accountId, long amount) {
